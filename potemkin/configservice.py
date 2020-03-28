1192da29645398748e230b03a562a3f911d9e778
todd wells <todd@wells.ws>
2020-03-28 12:15:23 -0700

    clean up

--- potemkin/configservice.py
+++ potemkin/configservice.py
@@ -4,45 +4,22 @@ import time
 MAX_ATTEMPTS = 50
 WAIT_PERIOD = 20
 
+
 def all_rule_results(configservice, rule_name):
     """ Return details for the given config rule, and deal with slurping all the results
 
     :param configservice: boto client for AWS Config
     :param rule_name: name of rule to get compliance details for
     :returns: slurped version of get_compliance_details_by_config_rule response """
-    paginator = configservice.get_paginator('get_compliance_details_by_config_rule')
+    paginator = configservice.get_paginator(
+        'get_compliance_details_by_config_rule')
     page_iterator = paginator.paginate(
         ConfigRuleName=rule_name,
-        ComplianceTypes=[
-            'NON_COMPLIANT',
-            'COMPLIANT',
-            'NOT_APPLICABLE'
-        ]
-    )
+        ComplianceTypes=['NON_COMPLIANT', 'COMPLIANT', 'NOT_APPLICABLE'])
     return [
-        evaluation_result
-        for page in page_iterator
+        evaluation_result for page in page_iterator
         for evaluation_result in page['EvaluationResults']
     ]
-
-
-def wait_for_resources(configservice, resource_id, rule_name):
-    attempts = 0
-    while True:
-        compliance_result = [
-            result for result in all_rule_results(configservice, rule_name)
-            if result['EvaluationResultIdentifier']
-            ['EvaluationResultQualifier']['ResourceId'] == resource_id
-        ]
-        if compliance_result:
-            print(f'compliance_result {compliance_result}')
-            return compliance_result[0]
-        else:
-            attempts += 1
-            if attempts == MAX_ATTEMPTS:
-                return None
-            else:
-                time.sleep(WAIT_PERIOD)
 
 
 def config_rule_wait_for_resource(configservice, resource_id, rule_name):
@@ -63,10 +40,26 @@ def config_rule_wait_for_resource(configservice, resource_id, rule_name):
     :return: None if resource never shows up, otherwise the EvaluationResult from call to
              get_compliance_details_by_config_rule
     """
-    return wait_for_resources(configservice, resource_id, rule_name)
+    attempts = 0
+    while True:
+        compliance_result = [
+            result for result in all_rule_results(configservice, rule_name)
+            if result['EvaluationResultIdentifier']
+            ['EvaluationResultQualifier']['ResourceId'] == resource_id
+        ]
+        if compliance_result:
+            print(f'compliance_result {compliance_result}')
+            return compliance_result[0]
+        else:
+            attempts += 1
+            if attempts == MAX_ATTEMPTS:
+                return None
+            else:
+                time.sleep(WAIT_PERIOD)
 
 
-def evaluate_config_rule_and_wait_for_resource(configservice, resource_id, rule_name):
+def evaluate_config_rule_and_wait_for_resource(configservice, resource_id,
+                                               rule_name):
     """ Kick off the specified rule and wait for the resource_id to show up in the results.
     It's up to you to ensure that the rule and resource are relevant to each other... if not
     this thing will loop for a godawful long time.
@@ -86,12 +79,9 @@ def evaluate_config_rule_and_wait_for_resource(configservice, resource_id, rule_
     """
     try:
         _ = configservice.start_config_rules_evaluation(
-            ConfigRuleNames=[
-                rule_name
-            ]
-        )
+            ConfigRuleNames=[rule_name])
     except configservice.exceptions.LimitExceededException:
         # if throttled, just wait anyways
         pass
 
-    return wait_for_resources(configservice, resource_id, rule_name)
+    return config_rule_wait_for_resource(configservice, resource_id, rule_name)
